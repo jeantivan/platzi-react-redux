7b5e6bb417ce6d7afc4bd2c0e7e573310d416fee
Jean Paul Tivan <jptivan53@gmail.com>
2023-02-16 01:05:37 -0400

    Clase 6: Middlewares

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
 import App from "./App";
+import { featuring, logger } from "./middlewares";
 
 import { pokemonsReducer } from "./reducers/pokemons";
 
-const store = createStore(
-  pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
 );
+
+const store = createStore(pokemonsReducer, composeEnhancers);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
